1b586486cc07b5deede6460e2de515f5b24d8145
XiaoQu <qulianqing1314@qq.com>
2022-07-13 21:24:17 +0800

    feat: #n/a ] add new job

---
+++ src/main/java/com/xiaoqu/git/log/extract/DataStreamWebApiJob.java
@@ -0,0 +1,27 @@
+package com.xiaoqu.git.log.extract;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.StringUtils;
+
+import java.net.URL;
+
+public class DataStreamWebApiJob {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        URL resource = DataStreamWebApiJob.class.getClassLoader().getResource("git_log_secret.txt");
+        String filePath = resource.getPath();
+
+        DataStreamSource<String> stringDataStreamSource = env.readTextFile(filePath);
+        DataStream<Tuple2<CommitLog, String>> map = stringDataStreamSource
+                .filter(s -> !StringUtils.isNullOrWhitespaceOnly(s))
+                .map(String::trim)
+                .map(new Mapper());
+
+        map.addSink(new SinkToMysql());
+
+        env.execute("Flink Java API Skeleton");
+    }
+}
