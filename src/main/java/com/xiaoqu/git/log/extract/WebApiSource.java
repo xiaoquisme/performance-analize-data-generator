0e067e928c1293e2db2a2b4759e9da966a39aeca
XiaoQu <qulianqing1314@qq.com>
2022-07-14 16:04:13 +0800

    feat: #n/a feat: finish get all commit from github api

--- src/main/java/com/xiaoqu/git/log/extract/WebApiSource.java
+++ src/main/java/com/xiaoqu/git/log/extract/WebApiSource.java
@@ -2,8 +2,10 @@ package com.xiaoqu.git.log.extract;
 
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.type.TypeReference;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectReader;
 import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -12,15 +14,27 @@ import java.util.List;
 public class WebApiSource extends RichSourceFunction<GitResponse> {
     @Override
     public void run(SourceContext<GitResponse> ctx) throws Exception {
-        URL url = new URL("https://api.github.com/repos/SeaQL/sea-orm/commits");
-        HttpURLConnection myURLConnection = (HttpURLConnection)url.openConnection();
+        String path = "https://api.github.com/repos/SeaQL/sea-orm/commits?page=%s&per_page=100";
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectReader objectReader = objectMapper.readerFor(new TypeReference<List<GitResponse>>() {});
+        int pageCounter = 1;
+        while (true) {
+            String newPath = String.format(path, pageCounter++);
+            InputStream inputStream = sendRequest(newPath);
+            List<GitResponse> response = objectReader.readValue(inputStream);
+            if (response.isEmpty()) {
+                break;
+            }
+            response.forEach(ctx::collect);
+        }
+    }
+
+    private InputStream sendRequest(String path) throws IOException {
+        URL url = new URL(path);
+        HttpURLConnection myURLConnection = (HttpURLConnection) url.openConnection();
         myURLConnection.setRequestProperty("Authorization", "Bearer <TOKEN>");
         myURLConnection.setRequestMethod("GET");
-        InputStream responseMessage = myURLConnection.getInputStream();
-        ObjectMapper objectMapper = new ObjectMapper();
-        List<GitResponse> response = objectMapper.readerFor(new TypeReference<List<GitResponse>>() {})
-                        .readValue(responseMessage);
-        response.stream().parallel().forEach(ctx::collect);
+        return myURLConnection.getInputStream();
     }
 
     @Override
