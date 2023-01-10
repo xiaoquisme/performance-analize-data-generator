8bc7a8cdd8d9d9d088490d234f2548e89071c438
XiaoQu <1483523635@qq.com>
2023-01-10 21:56:30 +0800

    fix: fix NPE error.

--- src/main/java/com/xiaoqu/git/log/extract/webapi/jira/board/issue/JiraIssueResponse.java
+++ src/main/java/com/xiaoqu/git/log/extract/webapi/jira/board/issue/JiraIssueResponse.java
@@ -2,6 +2,7 @@ package com.xiaoqu.git.log.extract.webapi.jira.board.issue;
 
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.Collections;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -9,7 +10,7 @@ public class JiraIssueResponse {
     public int startAt;
     public int total;
     public int maxResults;
-    public List<JiraIssue> issues;
+    public List<JiraIssue> issues = Collections.emptyList();
     public boolean hasNext() {
         return issues.size() > 0;
     }
