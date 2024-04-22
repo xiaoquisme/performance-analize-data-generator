95e9e646e88175d02f4ef3dcf994fc1f51c59387
XiaoQu <1483523635@qq.com>
2024-04-22 10:37:25 +0800

    fix: fix file not found

--- src/main/java/com/xiaoqu/git/log/extract/common/SystemConfigLoader.java
+++ src/main/java/com/xiaoqu/git/log/extract/common/SystemConfigLoader.java
@@ -5,6 +5,8 @@ import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class SystemConfigLoader {
     public static SystemConfig config;
@@ -14,6 +16,8 @@ public class SystemConfigLoader {
         config =  yaml.load(resourceAsStream);
     }
     public static HikariConfig getHikariConfig() {
-        return new HikariConfig("hikari.properties");
+        // get file path from hikari.properties
+        Path path = Paths.get(SystemConfigLoader.class.getClassLoader().getResource("hikari.properties").getPath());
+        return new HikariConfig(path.toString());
     }
 }
